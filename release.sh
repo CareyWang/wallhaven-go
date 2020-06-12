f986454cbc89b40a5512cf3dabcec256b90d36b9
CareyWong <xidiancc@gmail.com>
2020-06-12 16:59:50 +0800

    Modify release file name

--- release.sh
+++ release.sh
@@ -2,6 +2,6 @@
 
 go mod tidy
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/whs-linux-amd64.service main.go
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o build/whs-darwin-amd64.service main.go
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build/whs-windows-amd64.service main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/whg-linux-amd64.service main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o build/whg-darwin-amd64.service main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build/whg-windows-amd64.service main.go
